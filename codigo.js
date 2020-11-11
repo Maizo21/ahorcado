3fc32a34bc4678d5fdda9b55172add0ed17ba727
Hernan Amaiz <hernanamaizp@gmail.com>
2020-11-11 00:02:48 -0300

    primer commit

---
+++ codigo.js
@@ -0,0 +1,130 @@
+let palabras = [
+    'tomate', 'clima', 'programa', 'adivinanza', 'espeluznante',
+    'bosque', 'increible', 'computador', 'guitarra', 'aspiradora',
+    'canto', 'poligono', 'licuadora', 'mascarilla', 'murcielago',
+    'lluvia', 'mantenimiento', 'longitud', 'espectro', 'esfuerzo',
+    'velocidad', 'volcan', 'victorioso', 'madriguera', 'poderoso',
+    'perfume','teclado','medidas','porcentaje','refrigerador',
+    'paralelepipedos','antibioticos','adaptacion','estimulacion',
+    'funcional','fisico','diagnostico','ligamento','nervios','pantorrilla',
+    'pulcritud'
+]
+
+let palabraIncognita = palabras[Math.floor(Math.random()*palabras.length)]
+let numeroLetras = palabraIncognita.length;
+let letrasIncognitas = [];
+let vidas = 9;
+let ahorcado = document.getElementById('juego-img')
+
+function rendirse(e){
+    e.preventDefault()
+    swal.fire({
+        title:'Game Over!', 
+        width:'500px',
+        text: `La palabra era '${palabraIncognita}'`,
+        confirmButtonText: 'Ok',
+        confirmButtonColor: 'black',
+        background: `
+        url('gback.gif')
+        center`
+    })
+    .then((value) => {
+        if(value.isConfirmed){
+            location.reload();
+        }
+    })
+}
+
+function letras(e){
+
+    var letraIngresada = document.getElementById('letra').value;
+    e.preventDefault()
+    form = document.getElementById("form").reset();
+
+    if(letraIngresada === ''){
+        swal.fire({
+            title:'Ingrese una Letra', 
+            width:'500px',
+            confirmButtonText: 'Ok',
+            confirmButtonColor: 'black',
+            confirmButtonColor: 'black'
+        })
+
+    }else if(palabraIncognita.indexOf(letraIngresada) === -1){
+        vidas--;
+
+        ahorcado.innerHTML = (`<img src = ${vidas}.png id='juego-img-ahorcado'>`)
+
+            if(vidas===0){
+                swal.fire({
+                    title:'Game Over!', 
+                    width:'500px',
+                    showDenyButton: true,
+                    denyButtonText:'Rendirse',
+                    denyButtonColor:'black',
+                    confirmButtonText: 'Jugar de nuevo',
+                    confirmButtonColor: 'black',
+                    background: `
+                    url('gback.gif')
+                    center`})
+                .then((value) => {
+                    if(value.isConfirmed){
+                        location.reload();
+                    }
+                    else{
+                        swal.fire({
+                        title:'Game Over!', 
+                        width:'500px',
+                        text: `La palabra era '${palabraIncognita}'`,
+                        confirmButtonText: 'Ok',
+                        confirmButtonColor: 'black',
+                        background: `
+                        url('gback.gif')
+                        center`})  
+                        
+                    }
+                })
+            }
+            
+
+    }else{
+        for(var j = 0; j < palabraIncognita.length; j++){
+            if(palabraIncognita[j] === letraIngresada){
+            letrasIncognitas[j] = letraIngresada
+            numeroLetras--;
+            respuesta.innerHTML = (letrasIncognitas.join(''))
+                if(numeroLetras === 0){
+                    swal.fire({
+                        title:'GANADOR', 
+                        text:'¿Deseas jugar de nuevo?',
+                        width:'500px',
+                        showDenyButton: true,
+                        denyButtonText:'No',
+                        denyButtonColor:'black',
+                        confirmButtonText: 'Si',
+                        confirmButtonColor: 'black',})
+                    .then((value) => {
+    
+                        if(value.isConfirmed){
+                            location.reload();
+                        }
+                    })
+                }
+            }
+        }
+    }
+
+}
+
+
+
+for (let i = 0; i < palabraIncognita.length; i++){
+    letrasIncognitas[i] = '_'
+    var respuesta = document.getElementById('juego-respuesta');
+    respuesta.innerHTML = (letrasIncognitas.join(''))
+}
+
+let b2 = document.getElementById('b2')
+b2.addEventListener('click', rendirse)
+let b1 = document.getElementById('b1')
+b1.addEventListener('click', letras)
